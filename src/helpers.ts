b2d021b3f0894eb6072392313cb1b9775f17f9cb
Andreas Riedmüller <andreas@simplease.at>
2024-09-27 12:31:10 +0200

    fix: update icons

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,17 +3,15 @@ import Browser from 'webextension-polyfill'
 export const actionIconDefault: Browser.Action.SetIconDetailsType = {
   path: {
     16: 'images/icon-inactive-16.png',
+    24: 'images/icon-inactive-24.png',
     32: 'images/icon-inactive-32.png',
-    48: 'images/icon-inactive-48.png',
-    128: 'images/icon-inactive-128.png'
   }
 }
 
 export const actionIconActive: Browser.Action.SetIconDetailsType = {
   path: {
     16: 'images/icon-16.png',
+    24: 'images/icon-24.png',
     32: 'images/icon-32.png',
-    48: 'images/icon-48.png',
-    128: 'images/icon-128.png'
   }
 }
